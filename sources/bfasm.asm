1d128c9f0530931a079a8433060cf97cb7e64ff0
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-01 15:08:52 +0100

    Update bfasm.asm

--- sources/bfasm.asm
+++ sources/bfasm.asm
@@ -1,3 +1,12 @@
+;; bfasm.asm: Copyright (C) 2020-2021 Timo Sarkar <sartimo10@gmail.com>
+;; Licensed under the terms of the Creative Commons License, either
+;; version 2 or (at your option) any later version.
+;;
+;; To build:
+;;	nasm -f bin -o bfasm bfasm.asm
+;; To use:
+;;	bfasm < foo.bf > foo.out
+
 BITS 32
 
 %define arraysize    30000
